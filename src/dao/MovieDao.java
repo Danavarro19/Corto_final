1e23040e1c5577db8a2a5d97b797a90a12a8af75
intel <intel@192.168.100.8>
2018-06-20 15:06:09 -0600

    commit

---
+++ src/dao/MovieDao.java
@@ -0,0 +1,139 @@
+package dao;
+
+import conexion.Conexion;
+import interfaces.Metodos;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import modelo.Movie;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class MovieDao implements Metodos<Movie>{
+    
+    private static final String SQL_INSERT="INSERT INTO movie (nombre,director,pais,clasificacion,anio,en_proyeccion)VALUES(?,?,?,?,?,?)";
+    private static final String SQL_UPDATE="UPDATE movie SET en_proyeccion=? WHERE nombre=?";
+    private static final String SQL_DELETE="DELETE FROM movies WHERE nombre=?";
+    private static final String SQL_READ="SELECT * FROM movies WHERE nombre=?";
+    private static final String SQL_READALL="SELECT * FROM movie";
+    private static final Conexion con = Conexion.conectar();
+
+    @Override
+    public boolean create(Movie g) {
+        PreparedStatement ps;
+        try{
+            ps =con.getCnx().prepareStatement(SQL_INSERT);
+            ps.setString(1, g.getNombre());
+            ps.setString(2,g.getDirector());
+            ps.setString(3,g.getPaisProcedencia());
+            ps.setString(4,g.getClasificacion());
+            ps.setInt(5,g.getAnnioEstreno());
+            ps.setInt(6,1);
+            if(ps.executeUpdate()>0)
+                return true;
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        
+        }finally {
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        PreparedStatement ps;
+        try{
+            ps=con.getCnx().prepareStatement(SQL_DELETE);
+            ps.setString(1, key.toString());
+            
+            if(ps.executeUpdate()>0)
+                return true;
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            con.cerrarConexion();
+        }
+        return false; 
+    }
+
+    @Override
+    public boolean update(Movie c) {
+        PreparedStatement ps;
+        
+        try{
+            ps = con.getCnx().prepareStatement(SQL_UPDATE);
+            ps.setString(1, Integer.toString(c.EnExhibicion()));
+            ps.setString(2, c.getNombre());
+            if (ps.executeUpdate()>0)
+                return true;
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            con.cerrarConexion();
+        }
+        return false;
+
+    }
+
+    @Override
+    public Movie read(Object key) {
+        Movie m =null;
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        try{
+            ps= con.getCnx().prepareStatement(SQL_READ);
+            ps.setString(1, key.toString());
+            
+            rs= ps.executeQuery();
+            
+            while(rs.next()){
+                m=new Movie(rs.getString(2),rs.getString(3),
+                rs.getString(4),rs.getString(5),rs.getInt(6), rs.getInt(7));
+            }
+            rs.close();
+        
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        
+        }finally{
+            con.cerrarConexion();
+        }    
+        return m;
+    
+    }
+
+    @Override
+    public ArrayList<Movie> readAll() {
+        ArrayList<Movie> all= new ArrayList();
+        Statement s;
+        ResultSet rs;
+        try{
+            s=con.getCnx().prepareStatement(SQL_READALL);
+            rs=s.executeQuery(SQL_READALL);
+            while(rs.next()){
+                all.add(new Movie(rs.getString(2),rs.getString(3),
+                        rs.getString(4),rs.getString(5),rs.getInt(6), rs.getInt(7)));
+            }
+            rs.close();
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            con.cerrarConexion();
+        }
+        
+        return all;
+    }
+    
+    
+    
+    
+}
